3a2db41b38be46e88eafc1ef909f44e971f7ece1
andrewduweling <andrewduweling@andrews-mbp-72>
2022-08-28 21:20:52 -0500

    Initial Commit

---
+++ src/Hello.java
@@ -0,0 +1,15 @@
+/**
+ * Author: Andrew Duweling (aduweling2@huskers.unl.edu) 
+ * Date: 08/28/2022
+ * 
+ * A simple "Hello World!" program in Java
+ * 
+ * @author cbourke
+ *
+ */
+public class Hello {
+
+	public static void main(String args[]) {
+		System.out.println("Andrew Duweling");
+	}
+}
